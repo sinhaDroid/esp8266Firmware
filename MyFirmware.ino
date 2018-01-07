6e51a915f52c0ee6f860b818013328294e854e89
sinhaDroid <107sinha@gmail.com>
2018-01-07 19:39:16 +0530

    wifi connection and no of station connected

--- MyFirmware.ino
+++ MyFirmware.ino
@@ -38,7 +38,15 @@
 const char *ssid = "ESPap";
 const char *password = "thereisnospoon";
 
+const char* host;
+const int httpPort = 80;
+const int httpsPort = 443;
+
 ESP8266WebServer server(80);
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "CF 05 98 89 CA FF 8E D8 5E 5C E0 C2 E4 F7 E6 C3 C7 50 DD 5C";
 
 /* Just a little test message.  Go to http://192.168.4.1 in a web browser
  * connected to this access point to see it.
@@ -47,49 +55,52 @@ void handleRoot() {
 	server.send(200, "text/html", "<h1>You are connected</h1>");
 }
 
+void handleRequest() {
+  String request = "{\"unlocked\":";
+  request += true;
+  request += "}";
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  
 	delay(1000);
+  
 	Serial.begin(115200);
 	Serial.println();
+  WiFi.mode(WIFI_AP);               //Only Access point
 	Serial.println("Configuring access point...");
 	/* You can remove the password parameter if you want the AP to be open. */
-	WiFi.softAP(ssid, password);
+	WiFi.softAP(ssid, password, 6, false);
   
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  // Check if a AP has connected
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start the server
-	server.begin();
-	Serial.println("HTTP server started");
-
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  server.on("/", handleRoot);
+
+  Serial.print("MAC: ");
+  Serial.println(WiFi.softAPmacAddress());
+
+  server.on("/", HTTP_POST, handleRoot);
+  
+  // Start the server
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on
-  // Check if a client has connected
-//  WiFiClient client = server.available();
-  
-//  if (client) {
-    // if client has connected then blink for every second
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
-//  }
+  int station_num = WiFi.softAPgetStationNum();
+
+  if (station_num > 0) {
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on
+    delay(1000);                      // Wait for a second
+    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+    delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on
+  }
   
 	server.handleClient();
 }
